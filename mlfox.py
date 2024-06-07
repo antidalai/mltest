620d626621bd16026a58d100d39874b1f183b3d6
antidalai <dalaibiju1@gmail.com>
2024-06-07 16:54:12 -0400

    for some reason laptop needs /mltest but pc doesnt

--- mlfox.py
+++ mlfox.py
@@ -15,7 +15,7 @@ print("Current Directory: ", os.getcwd())
 
 # This whole function is dedicated to displaying data
 def graphRawFX():
-    date,bid,ask = np.loadtxt('mltest/GBPUSD1d.txt', unpack = True, 
+    date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack = True, # need to add /mltest when on pc but not when on laptop
                               delimiter = ',', converters = {0: date_converter}) # This is using matplotlib.dates and stripping the string into proper date format
     fig = plt.figure(figure=(10,7)) # Essentially creates a blank canvas for drawing our plots
     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40) # Creates a sublot with certain specifications
