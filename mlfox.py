3f46feb63856ec7694245f192bcabf71c52ed09c
antidalai <dalaibiju1@gmail.com>
2024-06-08 10:49:58 -0400

    added percent change function

--- mlfox.py
+++ mlfox.py
@@ -7,16 +7,22 @@ import os
 from dateutil.parser import parse
 
 
+
 def date_converter(date_string):
     return mdates.date2num(parse(date_string))
 
+date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack = True,  # need to add /mltest when on pc but not when on laptop
+                              delimiter = ',', converters = {0: date_converter}) # This is using matplotlib.dates and stripping the string into proper date format
 
-print("Current Directory: ", os.getcwd())
+
+def percentChange(startPoint, currentPoint):
+    return ((currentPoint - startPoint) / startPoint) * 100 #   Calculate the percent change between the start and current
+
+
+print("This is the bid: ", bid)
 
 # This whole function is dedicated to displaying data
 def graphRawFX():
-    date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack = True,  # need to add /mltest when on pc but not when on laptop
-                              delimiter = ',', converters = {0: date_converter}) # This is using matplotlib.dates and stripping the string into proper date format
     fig = plt.figure(figure=(10,7))  # Essentially creates a blank canvas for drawing our plots
     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40) # Creates a sublot with certain specifications
     
