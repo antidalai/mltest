9282f88398bcefb84d151609628c053275f5c98b
antidalai <73662113+antidalai@users.noreply.github.com>
2024-06-11 16:52:00 -0400

    added patternRecognition, and currentPattern functions among other changes

--- mlfox.py
+++ mlfox.py
@@ -15,17 +15,27 @@ def date_converter(date_string):
 date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack = True,  # need to add /mltest when on pc but not when on laptop
                               delimiter = ',', converters = {0: date_converter}) # This is using matplotlib.dates and stripping the string into proper date format
 
+avgLine = (bid+ask)/2   # Finds the middle spot between bid and ask price
+patternArr = [] # As we run pattern finder we will store the patterns in this array
+performanceArr = [] 
+patForRecog = [] # pattern for recognition / current pattern type of thing
+
 
 def percentChange(startPoint, currentPoint):
-    return ((currentPoint - startPoint) / startPoint) * 100 #   Calculate the percent change between the start and current
+    return ((currentPoint - startPoint) / abs(startPoint)) * 100 #   Calculate the percent change between the start and current
 
 
-def patternFinder():
-    avgLine = (bid+ask)/2   # Finds the middle spot between bid and ask price
+# This function creates kind of an average change between 10 points
+# Also sets a future range of 10 points
+# Creates and stores patterns in percent change between 10 points
+def patternStorage():
+    patStartTime = time.time()
+
     x = len(avgLine) - 30
     y = 11
 
     while y < x:
+        pattern = []
         p1 = percentChange(avgLine[y-10], avgLine[y-9])
         p2 = percentChange(avgLine[y-10], avgLine[y-8])
         p3 = percentChange(avgLine[y-10], avgLine[y-7])
@@ -39,15 +49,48 @@ def patternFinder():
 
         outcomeRange = avgLine[y+20:y+30]
         currentPoint = avgLine[y]
+        try:
+            avgOutcome = sum(outcomeRange) / len(outcomeRange)
+        except Exception as e:
+            print(str(e))
+            avgOutcome = 0
 
-        print(sum(outcomeRange) / len(outcomeRange))
-        print(currentPoint)
-        print(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        futureOutcome = percentChange(currentPoint, avgOutcome)
+        pointsArr =[p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+        pattern.extend(pointsArr)
+        patternArr.append(pattern)
+        performanceArr.append(futureOutcome)
+
         y += 1
-        time.sleep(5555)
+    
+    patEndTime = time.time()
+    print(len(patternArr))
+    print(len(performanceArr))
+    print("Pattern storage took", patEndTime-patStartTime, ' seconds')
+
+def currentPattern():
+    currentPat1 = percentChange(avgLine[-11], avgLine[-10])
+    currentPat2 = percentChange(avgLine[-11], avgLine[-9])
+    currentPat3 = percentChange(avgLine[-11], avgLine[-8])
+    currentPat4 = percentChange(avgLine[-11], avgLine[-7])
+    currentPat5 = percentChange(avgLine[-11], avgLine[-6])
+    currentPat6 = percentChange(avgLine[-11], avgLine[-5])
+    currentPat7 = percentChange(avgLine[-11], avgLine[-4])
+    currentPat8 = percentChange(avgLine[-11], avgLine[-3])
+    currentPat9 = percentChange(avgLine[-11], avgLine[-2])
+    currentPat10 = percentChange(avgLine[-11], avgLine[-1])
+
+    listCurrentPats = [currentPat1, currentPat2, currentPat3, currentPat4, currentPat5, currentPat6, currentPat7, currentPat8, currentPat9, currentPat10]
+    patForRecog.extend(listCurrentPats)
+
+    print(patForRecog)
 
 
-#print("This is the bid: ", bid)
+
+def patternRecognition():
+    for myPattern in patternArr:
+        sim1 = percentChange(myPattern[0], patForRecog[0])
+
 
 # This whole function is dedicated to displaying data
 def graphRawFX():
@@ -68,5 +111,6 @@ def graphRawFX():
     plt.grid(True)
     plt.show()
 
-graphRawFX()
-patternFinder()
+#graphRawFX()
+patternStorage()
+currentPattern()
